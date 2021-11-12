702b655732a676c05f0f0a14de68bfd43ec4b86e
zarpli <43631223+zarpli@users.noreply.github.com>
2021-11-12 01:19:45 -0600

    Update loop.ino

--- examples/loop.ino
+++ examples/loop.ino
@@ -9,16 +9,13 @@ BS.debug();               // Enable debug msg over Serial
 Serial.begin(9600);
 while(!Serial);
 
-Serial.println("Wait BrightSign Online");
+Serial.println("Waiting BrightSign PowerUp");
 while(!BS.online())BS.update(); 
 
-BS.volume(20);
+BS.volume(20);            //Sel Volume to 20%
 }
 
 void loop(){
-
 BS.update();
-
 if(BS.media_ended()) BS.play("AUDIO.M4A");
-
 }
